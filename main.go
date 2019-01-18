fd087b6ba88cb1808404ced89d87acd2df909111
caidicaidi123 <caidicaidi123@gmail.com>
2019-01-18 20:38:08 +0800

    embedded struct

--- main.go
+++ main.go
@@ -2,17 +2,36 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	phone int
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName string
+	contact contactInfo
 }
 
 func main() {
 	// alex := person{firstName: "Alex", lastName: "Anderson"}
 	// fmt.Println(alex)
-	var alex person
-	alex.firstName = "Alex"
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	// var alex person
+	// alex.firstName = "Alex"
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+	// fmt.Println()
+	jim := person{
+		"Jim",
+		"Party",
+		contactInfo{
+			"jim@gmail.com",
+			87651234,
+			123456,
+		},
+	}
+	
+	fmt.Printf("%+v", jim)
 	fmt.Println()
 }
